80d9e18c7d66df43c171c0800ce1143d50b8bd12
Liam Doyle <liamdoyle@limegorilla.co.uk>
2023-06-27 15:46:17 +0100

    much better cam
    Signed-off-by: Liam Doyle <liamdoyle@limegorilla.co.uk>

--- src/components/ui/Scanner.tsx
+++ src/components/ui/Scanner.tsx
@@ -1,30 +1,40 @@
 "use client";
 
 import { Html5Qrcode } from "html5-qrcode";
+import { useEffect, useState } from "react";
 
 export default function Scanner() {
-  const cam = Html5Qrcode.getCameras().then((devices) => {
-    if (devices && devices.length > 0) {
-      const cameraId = devices[0]?.id;
-      if (!cameraId) {
-        return;
-      }
-      const html5QrCode = new Html5Qrcode("reader");
+  const [activeCamera, setActiveCamera] = useState<string>("");
+  let html5QrCode: any;
+  let qrcodeId = "qr-code-scanner";
+  useEffect(() => {
+    // Anything in here is fired on component mount.
+    if (!html5QrCode?.getState()) {
+      html5QrCode = new Html5Qrcode(qrcodeId);
+      const qrCodeSuccessCallback = (
+        decodedText: string,
+        decodedResult: string
+      ) => {
+        /* handle success */
+      };
+      const config = {
+        fps: 10,
+        qrbox: { width: 250, height: 250 },
+        aspectRatio: 1.777778,
+      };
+
+      // If you want to prefer back camera
       html5QrCode.start(
-        cameraId,
-        {
-          fps: 10,
-          qrbox: 250,
-        },
-        (qrCode) => {
-          console.log(qrCode);
-        },
-        (errorMessage) => {
-          console.log(errorMessage);
-        }
+        { facingMode: "environment" },
+        config,
+        qrCodeSuccessCallback
       );
     }
-  });
 
-  return <div id="reader" className="w-full"></div>;
+    return () => {
+      // Anything in here is fired on component unmount.
+    };
+  }, []);
+
+  return <div id={qrcodeId} className="w-full"></div>;
 }
